507747802d7301ec2e99b6e2474344cf7e6c971b
Nori <kanjam8session@gmail.com>
2021-01-08 21:19:12 +0900

    Initial Commit

---
+++ ScheduleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScheduleApp
+//
+//  Created by Norihiro.Nakano on 2021/01/08.
+//  Copyright © 2021 Norihiro.Nakano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
